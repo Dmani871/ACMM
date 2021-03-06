0ac9be9fee446e46619c0b165d87629ca5aa12f8
Dmani <dmani.barnett@gmail.com>
2021-03-06 16:25:12 +0000

    Added Mentor Profile

--- ACMM/mentorship/models.py
+++ ACMM/mentorship/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+from django.contrib.postgres.fields import ArrayField
 
-# Create your models here.
+class MentorProfile(models.Model):
+    # personal details
+    email = models.EmailField(max_length=255, unique=True)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=150)
+    occupation= models.CharField(max_length=255,blank = True,null = True)
+    interests = ArrayField(
+        models.CharField(max_length=255),
+        blank = True,
+        null = True,
+        )
+    specialty= ArrayField(
+        models.CharField(max_length=255),
+        blank = True,
+        null = True,
+        )
+    application_strength= models.CharField(max_length=255,blank = True,null = True)
+    year_of_study = models.PositiveIntegerField(blank = True,null = True)
+    hear_about_us= models.CharField(max_length=255,blank = True,null = True)
+    is_active = models.BooleanField(
+        default=True,
+    )
+    entrance_exam_experience=ArrayField(
+        models.CharField(max_length=100, blank=True),
+        blank = True,
+        null = True,
+        )
+    interview_experience=ArrayField(
+        models.CharField(max_length=100, blank=True),
+        blank = True,
+        null = True,
+        )
+    year_applied=models.CharField(max_length=255,blank = True,null = True)
+    
+    
+    def __str__(self): 
+        return self.email
