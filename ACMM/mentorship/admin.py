2936fff2171958dfb8a125a64fc47fbbdc3fa4b7
Dmani <dmani.barnett@gmail.com>
2021-04-29 00:24:31 +0100

    Removed export

--- ACMM/mentorship/admin.py
+++ ACMM/mentorship/admin.py
@@ -3,7 +3,6 @@ import csv
 from django.http import HttpResponse
 from .models import MentorProfile,MenteeProfile,MentorQualification,MenteeQualification
 from .forms import MentorForm,MenteeForm
-# Register your models here.
 from django.forms import formset_factory
 from django.forms import inlineformset_factory
 from collections import defaultdict
@@ -37,26 +36,6 @@ class MenteeAdmin(admin.ModelAdmin):
     search_fields = ['first_name','last_name','email']
     list_filter = ['course','date_joined','accepted','sex','year_applied']
     list_display = ['first_name','last_name','email','mentor']
-    
-    
-    def export_as_csv(self, request, queryset):
-        meta = self.model._meta
-        field_names = [field.name for field in meta.fields]
-        column_names= field_names+["Qualifications"]
-        response = HttpResponse(content_type='text/csv')
-        response['Content-Disposition'] = 'attachment; filename={}.csv'.format(meta)
-        writer = csv.writer(response)
-
-        writer.writerow(column_names)
-
-        for obj in queryset:
-            row_contents=[getattr(obj, field) for field in field_names]
-            qualifications=[(q.name,q.education_level,q.grade,q.predicted) for q in MenteeQualification.objects.filter(profile=obj)]
-            row_contents.append(qualifications)
-            row = writer.writerow(row_contents)
-        return response
-
-    export_as_csv.short_description = "Export Selected Profiles"
     
     
     def assign_mentor(self, request, queryset):
@@ -102,7 +81,7 @@ class MenteeAdmin(admin.ModelAdmin):
 
 
     assign_mentor.short_description = "Assign a mentor to each mentee"
-    actions = ["export_as_csv","assign_mentor"]
+    actions = ["assign_mentor"]
 
 
 admin.site.register(MentorProfile,MentorAdmin)
