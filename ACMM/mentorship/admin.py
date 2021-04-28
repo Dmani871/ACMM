9c2a8f972ecdfbbf935ec5772da9f29877a8d1cf
Dmani <dmani.barnett@gmail.com>
2021-04-29 00:22:54 +0100

    WIP basic matching

--- ACMM/mentorship/admin.py
+++ ACMM/mentorship/admin.py
@@ -8,6 +8,7 @@ from django.forms import formset_factory
 from django.forms import inlineformset_factory
 from collections import defaultdict
 import numpy as np
+import pandas as pd
 
 class MentorQualificationInline(admin.TabularInline):
     model = MentorQualification
@@ -60,73 +61,46 @@ class MenteeAdmin(admin.ModelAdmin):
     
     def assign_mentor(self, request, queryset):
         available_mentors = MentorProfile.objects.filter(is_active=True,menteeprofile__isnull=True)
-        mentee_preferences=defaultdict(list)
+        mentee_preferences=defaultdict(dict)
+        mentor_preferences=defaultdict(list)
         for mentee in queryset:
-            matches=[]
+            matches=defaultdict(dict)
             for mentor in available_mentors:
                 ranking=0
                 if mentee.course == mentor.occupation[0]:
                     ranking=10
                 if mentor.year_applied==mentee.year_applied:
                     ranking *=2
+                if mentor.sex!=mentee.sex:
+                    ranking*=0.8
                 
                 support_factor = len(np.intersect1d(mentor.area_of_support,mentee.area_of_support))+1
                 interview_factor = len(np.intersect1d(mentor.interview_experience,mentee.interview_experience))
                 exam_factor = len(np.intersect1d(mentor.entrance_exam_experience,mentee.entrance_exam_experience))
+                if 'EE' in mentee.area_of_support:
+                    exam_factor *=2
+                if 'I' in mentee.area_of_support:
+                    interview_factor *=2
                 mentee_qualifications=[q.name for q in MenteeQualification.objects.filter(profile=mentee)]
                 mentor_qualifications=[q.name for q in MentorQualification.objects.filter(profile=mentor)]
                 subject_factor = len(np.intersect1d(mentor_qualifications,mentee_qualifications))
                 mentor_factor =1+((exam_factor+interview_factor+subject_factor)*0.1)
                 ranking=(ranking*support_factor)*mentor_factor
-                if mentor.sex!=mentee.sex:
-                    ranking*=0.8
+                
 
                 if ranking >0:
-                    matches.append({mentor.id:ranking})
+                    matches[mentor.id]=ranking
             mentee_preferences[mentee.id]=matches
-        for store, stuff in mentee_preferences.items():
-            print( store )
-            print( stuff )
-                
-        """)
-        print(dir(available_mentors[0]))
-        print(available_mentors[0])
-        print(available_mentors[0].menteeprofile)
-        print(hasattr(available_mentors[1],'menteeprofile'))"""
-        """
-        for mentee in queryset:
-            match=[None,0]
-            for mentor in available_mentors:
-                ranking=0
-                if mentee.course in ["Medicine","Graduate Medicine"] and not "Dent" in mentor.occupation:
-                    ranking=100000
-                elif mentee.course=="Dentistry" and "Dent" in mentor.occupation:
-                    ranking=100000
-                if mentor.year_applied==mentee.year_applied:
-                    ranking=ranking*100
-                if mentor.application_strength in mentee.area_of_support:
-                    ranking=ranking*2
-                if mentor.sex!=mentee.sex:
-                    ranking=ranking*0.7
-                entrance_exam_experience_factor = len(set.intersection(set(mentor.entrance_exam_experience), set(mentee.entrance_exam_experience)))+1
-                interview_experience_factor = len(set.intersection(set(mentor.interview_experience), set(mentee.interview_experience)))+1
-                support_factor = len(set.intersection(set(mentor.area_of_support), set(mentee.area_of_support)))+1
-                mentee_qualifications=[q.name for q in MenteeQualification.objects.filter(profile=mentee)]
-                mentor_qualifications=[q.name for q in MentorQualification.objects.filter(profile=mentor)]
-                subject_factor = len(set.intersection(set(mentor_qualifications), set(mentee_qualifications)))+1
-                ranking=ranking*entrance_exam_experience_factor*interview_experience_factor*support_factor*subject_factor
-                
-                
-                if ranking > match[1]:
-                    match[0]=mentor
-                    match[1]=ranking
+        
+        matches_df=pd.DataFrame(mentee_preferences)  
+        matches=matches_df.idxmax(axis=1)
+        for mentor_id,mentee_id in matches.items():
+            mentee=MenteeProfile.objects.get(id=mentee_id)
+            mentor=MentorProfile.objects.get(id=mentor_id)
+            mentee.mentor=mentor
+            mentee.save()
 
-            if match[0] != None:
-                available_mentors.remove(match[0])
-                mentee.assigned_mentor=match[0]
-                mentee.save()
-        """
-        #for x in queryset:print(x);
+
     assign_mentor.short_description = "Assign a mentor to each mentee"
     actions = ["export_as_csv","assign_mentor"]
 
