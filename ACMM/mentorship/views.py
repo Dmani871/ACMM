359c8accbd9205a2cb1eaf302b63f362791e8b29
Dmani <dmani.barnett@gmail.com>
2021-04-30 21:03:26 +0100

    Clean up views and templates

--- ACMM/mentorship/views.py
+++ ACMM/mentorship/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.shortcuts import redirect
 from .forms import MentorForm,MenteeForm,MentorQualificationFormSet,MenteeQualificationFormSet
 from django.forms import formset_factory
-from django.views.generic import TemplateView
+from django.template import loader
 
 def mentor_signup_view(request):
     if request.method == "POST":
@@ -10,17 +10,16 @@ def mentor_signup_view(request):
         if form.is_valid():
             profile=form.save(commit=False)
             formset = MentorQualificationFormSet(request.POST, request.FILES,instance=profile)
-            print(formset)
             if formset.is_valid():
                 form.save()
                 formset.save()
                 return redirect('/thanks')
         formset = MentorQualificationFormSet(request.POST, request.FILES)
-        return render(request,  'mentor_signup.html', {'page_title':'Mentor Application',"form": form,"qualification_formset": formset})
+        return render(request,  'mentorship/mentor_signup.html', {'page_title':'Mentor Application',"form": form,"qualification_formset": formset})
     else:
         form = MentorForm()
         formset = MentorQualificationFormSet()
-        return render(request,  'mentor_signup.html', {'page_title':'Mentor Application',"form": form,"qualification_formset": formset})
+        return render(request,  'mentorship/mentor_signup.html', {'page_title':'Mentor Application',"form": form,"qualification_formset": formset})
 
 def mentee_signup_view(request):
     if request.method == "POST":
@@ -35,17 +34,17 @@ def mentee_signup_view(request):
                 formset.save()
                 return redirect('/thanks')
         formset = MenteeQualificationFormSet(request.POST, request.FILES)
-        return render(request,  'mentee_signup.html', {'page_title':'Mentee Application',"form": form,"qualification_formset": formset})
+        return render(request,  'mentorship/mentee_signup.html', {'page_title':'Mentee Application',"form": form,"qualification_formset": formset})
     else:
         form = MenteeForm()
         formset = MenteeQualificationFormSet()
-        return render(request,  'mentee_signup.html', {'page_title':'Mentee Application',"form": form,"qualification_formset": formset})
+        return render(request,  'mentorship/mentee_signup.html', {'page_title':'Mentee Application',"form": form,"qualification_formset": formset})
 
 def thank_you_view(request):
-    return render(request,  'thank_you.html',{'page_title':'Thank You'})
+    return render(request,  'mentorship/thank_you.html',{'page_title':'Thank You'})
 
 def next_year_view(request):
-    return render(request,  'next_year.html',{'page_title':'Apply Next Year'})
+    return render(request,  'mentorship/next_year.html',{'page_title':'Apply Next Year'})
 
 def applications_view(request):
-    return render(request,  'applications.html',{'page_title':'ACMM Application Portal'})
+    return render(request,  'mentorship/applications.html',{'page_title':'ACMM Application Portal'})
