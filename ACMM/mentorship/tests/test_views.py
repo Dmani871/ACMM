7548612e50a876ea6dbc4639f91e9f46e05aa58b
Dmani <dmani.barnett@gmail.com>
2021-05-15 14:02:11 +0100

    Added Next Year View Test

--- ACMM/mentorship/tests/test_views.py
+++ ACMM/mentorship/tests/test_views.py
@@ -13,3 +13,15 @@ class ThankYouViewTest(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'mentorship/thank_you.html')
 
+class NextYearViewTest(TestCase):
+    def test_view_url_exists_at_desired_location(self):
+        response = self.client.get('/mentorship/next_year')
+        self.assertEqual(response.status_code, 200)
+    def test_view_url_accessible_by_name(self):
+        response = self.client.get(reverse("mentorship:next_year"))
+        self.assertEqual(response.status_code, 200)
+    def test_view_uses_correct_template(self):
+        response = self.client.get(reverse("mentorship:next_year"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'mentorship/next_year.html')
+
