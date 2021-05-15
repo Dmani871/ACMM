3df03192db323adbe24bedde654219d5896b6d5c
Dmani <dmani.barnett@gmail.com>
2021-05-15 22:58:45 +0100

    Added email configuration in development

--- ACMM/ACMM/settings/development.py
+++ ACMM/ACMM/settings/development.py
@@ -1,6 +1,6 @@
 from .base import *
 DEBUG = True
-
+ALLOWED_HOSTS = []
 DATABASES = {
     'default': {
         'ENGINE': os.environ.get('SQL_ENGINE'),
@@ -11,3 +11,11 @@ DATABASES = {
         'PORT': os.environ.get('SQL_PORT', ''),
     }
 }
+
+
+EMAIL_BACKEND = os.environ.get('EMAIL_BACKEND')
+EMAIL_HOST = os.environ.get('EMAIL_HOST')
+EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS',True)
+EMAIL_PORT = os.environ.get('EMAIL_PORT',587)
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER','dbsoftwaresoultions@gmail.com') 
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD','dbsoftwaresoultions@gmail.com')
