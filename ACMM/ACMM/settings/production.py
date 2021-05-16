5bb8ff045fbec2c0e241f9752fe039630b09b8da
Dmani <dmani.barnett@gmail.com>
2021-05-17 00:23:42 +0100

    Working deployment

--- ACMM/ACMM/settings/production.py
+++ ACMM/ACMM/settings/production.py
@@ -39,15 +39,39 @@ SESSION_COOKIE_AGE=3600
 OGGING = {
     'version': 1,
     'disable_existing_loggers': False,
-    'handlers': {
-        'console': {
-            'class': 'logging.StreamHandler',
+    'formatters': {
+        'verbose': {
+            'format': ('%(asctime)s [%(process)d] [%(levelname)s] ' +
+                       'pathname=%(pathname)s lineno=%(lineno)s ' +
+                       'funcname=%(funcName)s %(message)s'),
+            'datefmt': '%Y-%m-%d %H:%M:%S'
         },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        }
+    },
+    'handlers': {
+        'null': {
+            'level': 'DEBUG',
+            'class': 'logging.NullHandler',
+        },
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose'
+        }
     },
     'loggers': {
         'django': {
             'handlers': ['console'],
              'level': os.getenv('DJANGO_LOG_LEVEL', 'DEBUG'),
         },
+        'testlogger': {
+            'handlers': ['console'],
+            'level': 'INFO',
+        }
     },
 }
+
+DEBUG_PROPAGATE_EXCEPTIONS = True
+COMPRESS_ENABLED = os.environ.get('COMPRESS_ENABLED', False)
