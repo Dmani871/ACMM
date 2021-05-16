56f9156e850b506440be03fb5b5d0f8930c8a08f
Dmani <dmani.barnett@gmail.com>
2021-05-16 15:39:45 +0100

    Tidy up factory

--- ACMM/mentorship/tests/factories.py
+++ ACMM/mentorship/tests/factories.py
@@ -1,44 +1,36 @@
 import factory
 import factory.fuzzy
 import random
+import itertools
 from mentorship import models
 
+def generate_combinations(choices):
+    options = sum([list(map(list, itertools.combinations(choices, i))) for i in range(1,len(choices) + 1)], [])
+    return options
+def unpack_choices(choices):
+    return [c_tuple[0] for c_tuple in choices]
 
+entrance_exam_experience_opts=generate_combinations(models.ENTRANCE_EXAMS_TYPES)
+interview_experience_opts=generate_combinations(unpack_choices(models.INTERVIEW_EXPERIENCE_CHOICES))
+area_of_support_opts=generate_combinations(unpack_choices(models.SPECIALTY_CHOICES))
+course_opts=unpack_choices(models.COURSE_CHOICES)
+year_applied_opts=unpack_choices(models.YEAR_APPLIED_CHOICES)
+hear_about_us_opts=unpack_choices(models.HEAR_ABOUT_US_CHOICES)
+occupation_opts=unpack_choices(models.OCCUPATION_CHOICES)
+education_level_opt=unpack_choices(models.EDUCATION_LEVEL_CHOICES)
 class MenteeFactory(factory.Factory):
     class Meta:
         model = models.MenteeProfile
     first_name = factory.Faker('first_name')
     last_name = factory.Faker('last_name')
     email = factory.LazyAttribute(lambda user: '{}.{}@example.com'.format(user.first_name, user.last_name).lower())
-    sex = factory.fuzzy.FuzzyChoice(['M','F'])
-    year_applied = factory.fuzzy.FuzzyChoice(['A2','GRAD'])
-    hear_about_us = factory.fuzzy.FuzzyChoice(['WM','C','SM','SU','O'])
-    entrance_exam_experience= factory.fuzzy.FuzzyChoice([
-        ['UKCAT'],
-        ['GAMSAT'],
-        ['BMAT'],
-        ['BMAT','UKCAT'],
-        ['BMAT','GAMSAT'],
-        ['GAMSAT','UKCAT'],
-        ['BMAT','GAMSAT','UKCAT']
-        ])
-    interview_experience=factory.fuzzy.FuzzyChoice([
-        ['P'],
-        ['M'],
-        ['G'],
-        ['G','P'],
-        ['G','M'],
-        ['M','P'],
-        ['G','M','P']])
-    area_of_support=factory.fuzzy.FuzzyChoice([
-        ['PS'],
-        ['I'],
-        ['EE'],
-        ['WE'],
-        ['I','PS'],
-        ['EE','I'],
-        ['PS','EE','WE']])
-    course=factory.fuzzy.FuzzyChoice(['M','D'])
+    sex = factory.fuzzy.FuzzyChoice(models.SEX_TYPES)
+    year_applied = factory.fuzzy.FuzzyChoice(year_applied_opts)
+    hear_about_us = factory.fuzzy.FuzzyChoice(hear_about_us_opts)
+    entrance_exam_experience= factory.fuzzy.FuzzyChoice(entrance_exam_experience_opts)
+    interview_experience=factory.fuzzy.FuzzyChoice(interview_experience_opts)
+    area_of_support=factory.fuzzy.FuzzyChoice(area_of_support_opts)
+    course=factory.fuzzy.FuzzyChoice(course_opts)
     mentor_need=factory.fuzzy.FuzzyText(length=200)
     mentor_help=factory.fuzzy.FuzzyText(length=200)
     mentor_relationship=factory.fuzzy.FuzzyText(length=200)
@@ -65,8 +57,8 @@ class MenteeQualificationFactory(factory.Factory):
         'French',
         'Biomedical Science',
         'Psychology'])
-    education_level= factory.fuzzy.FuzzyChoice(['AS','A2','IB','UG','M','D'])
-    grade=factory.fuzzy.FuzzyChoice(["A*","A","B","C","D","E","F","1","2","3","4","5","6","7","1st","2:1","2:2","3rd"])
+    education_level= factory.fuzzy.FuzzyChoice(education_level_opt)
+    grade=factory.fuzzy.FuzzyChoice(models.GRADES)
     profile = factory.SubFactory(MenteeFactory)
 
 class MentorFactory(factory.Factory):
@@ -75,35 +67,13 @@ class MentorFactory(factory.Factory):
     first_name = factory.Faker('first_name')
     last_name = factory.Faker('last_name')
     email = factory.LazyAttribute(lambda user: '{}.{}@example.com'.format(user.first_name, user.last_name).lower())
-    sex = factory.fuzzy.FuzzyChoice(['M','F'])
-    year_applied = factory.fuzzy.FuzzyChoice(['A2','GRAD'])
-    hear_about_us = factory.fuzzy.FuzzyChoice(['WM','C','SM','SU','O'])
-    entrance_exam_experience= factory.fuzzy.FuzzyChoice([
-        ['UKCAT'],
-        ['GAMSAT'],
-        ['BMAT'],
-        ['BMAT','UKCAT'],
-        ['BMAT','GAMSAT'],
-        ['GAMSAT','UKCAT'],
-        ['BMAT','GAMSAT','UKCAT']
-        ])
-    interview_experience=factory.fuzzy.FuzzyChoice([
-        ['P'],
-        ['M'],
-        ['G'],
-        ['G','P'],
-        ['G','M'],
-        ['M','P'],
-        ['G','M','P']])
-    area_of_support=factory.fuzzy.FuzzyChoice([
-        ['PS'],
-        ['I'],
-        ['EE'],
-        ['WE'],
-        ['I','PS'],
-        ['EE','I'],
-        ['PS','EE','WE']])
-    occupation=factory.fuzzy.FuzzyChoice(['MD','D','MS','DS'])
+    sex = factory.fuzzy.FuzzyChoice(models.SEX_TYPES)
+    year_applied = factory.fuzzy.FuzzyChoice(year_applied_opts)
+    hear_about_us = factory.fuzzy.FuzzyChoice(hear_about_us_opts)
+    entrance_exam_experience= factory.fuzzy.FuzzyChoice(entrance_exam_experience_opts)
+    interview_experience=factory.fuzzy.FuzzyChoice(interview_experience_opts)
+    area_of_support=factory.fuzzy.FuzzyChoice(area_of_support_opts)
+    occupation=factory.fuzzy.FuzzyChoice(occupation_opts)
     @factory.post_generation
     def qualifications(obj, create, extracted, **kwargs):
         if not create:
@@ -128,5 +98,5 @@ class MentorQualificationFactory(factory.Factory):
         'French',
         'Biomedical Science',
         'Psychology'])
-    education_level= factory.fuzzy.FuzzyChoice(['AS','A2','IB','UG','M','D'])
+    education_level= factory.fuzzy.FuzzyChoice(education_level_opt)
     profile = factory.SubFactory(MenteeFactory)
