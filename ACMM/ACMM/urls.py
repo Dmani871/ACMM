7e1a22af7fbccb7b7284ff3bd932beec90676ba5
Dmani <dmani.barnett@gmail.com>
2021-05-16 03:37:16 +0100

    Added auth with secret key

--- ACMM/ACMM/urls.py
+++ ACMM/ACMM/urls.py
@@ -3,10 +3,11 @@ from django.contrib import admin
 from django.urls import include, path
 from two_factor.urls import urlpatterns as tf_urls
 
+
 import os
 
 urlpatterns = [
-    path('', include(tf_urls)),
+    path(os.getenv('SECRET_ADMIN_URL')+'auth/', include(tf_urls)),
     path('mentorship/', include('mentorship.urls')),
     path('admin/',include('admin_honeypot.urls', namespace='admin_honeypot')),
     path(os.getenv('SECRET_ADMIN_URL') + 'admin/', admin.site.urls),
