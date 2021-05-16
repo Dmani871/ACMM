27d6be29fbc8f2af99e081c070e38cc046e282e9
Dmani <dmani.barnett@gmail.com>
2021-05-16 03:01:10 +0100

    Added 2FA

--- ACMM/ACMM/urls.py
+++ ACMM/ACMM/urls.py
@@ -1,12 +1,15 @@
 
 from django.contrib import admin
 from django.urls import include, path
+from two_factor.urls import urlpatterns as tf_urls
+
 import os
 
 urlpatterns = [
+    path('', include(tf_urls)),
     path('mentorship/', include('mentorship.urls')),
     path('admin/',include('admin_honeypot.urls', namespace='admin_honeypot')),
-    path(os.getenv('SECRET_ADMIN_URL') + '/admin/', admin.site.urls),
+    path(os.getenv('SECRET_ADMIN_URL') + 'admin/', admin.site.urls),
 ]
 
 admin.site.site_header = "ACMM Admin"
