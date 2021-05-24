dbac7db48f2c68ebe995936f9c72a28b9d7c4e9b
Dmani <dmani.barnett@gmail.com>
2021-05-24 01:01:31 +0100

    Updated production settings

--- ACMM/ACMM/settings/production.py
+++ ACMM/ACMM/settings/production.py
@@ -5,7 +5,7 @@ ALLOWED_HOSTS = ['acmm.herokuapp.com','localhost','127.0.0.1']
 DATABASES = {}
 db_from_env = dj_database_url.config(conn_max_age=600)
 DATABASES['default']=db_from_env
-
+#PMD5uytHB0wzduoL
 EMAIL_BACKEND = os.environ.get('EMAIL_BACKEND')
 EMAIL_HOST = os.environ.get('EMAIL_HOST')
 EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS',True)
@@ -29,43 +29,6 @@ GDPR_CAN_ANONYMISE_DATABASE = True
 #Session
 SESSION_EXPIRE_AT_BROWSER_CLOSE=True
 SESSION_COOKIE_AGE=3600
-
-OGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'formatters': {
-        'verbose': {
-            'format': ('%(asctime)s [%(process)d] [%(levelname)s] ' +
-                       'pathname=%(pathname)s lineno=%(lineno)s ' +
-                       'funcname=%(funcName)s %(message)s'),
-            'datefmt': '%Y-%m-%d %H:%M:%S'
-        },
-        'simple': {
-            'format': '%(levelname)s %(message)s'
-        }
-    },
-    'handlers': {
-        'null': {
-            'level': 'DEBUG',
-            'class': 'logging.NullHandler',
-        },
-        'console': {
-            'level': 'DEBUG',
-            'class': 'logging.StreamHandler',
-            'formatter': 'verbose'
-        }
-    },
-    'loggers': {
-        'django': {
-            'handlers': ['console'],
-             'level': os.getenv('DJANGO_LOG_LEVEL', 'DEBUG'),
-        },
-        'testlogger': {
-            'handlers': ['console'],
-            'level': 'INFO',
-        }
-    },
-}
 
 DEBUG_PROPAGATE_EXCEPTIONS = True
 COMPRESS_ENABLED = os.environ.get('COMPRESS_ENABLED', False)
