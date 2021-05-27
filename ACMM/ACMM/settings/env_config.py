254b0ba1532f27beee97e0c8bbb5795e798d780d
Dmani <dmani.barnett@gmail.com>
2021-05-27 15:24:36 +0100

    Cleaned up env_config

--- ACMM/ACMM/settings/env_config.py
+++ ACMM/ACMM/settings/env_config.py
@@ -10,13 +10,11 @@ env = environ.Env()
 if os.path.isfile(env_file):
     env.read_env(env_file)
 else:
-    # [START cloudrun_django_secretconfig]
     import google.auth
     from google.cloud import secretmanager as sm
-    SETTINGS_NAME = "application_settings"
+    SETTINGS_NAME = "django_settings"
     _, project = google.auth.default()
     client = sm.SecretManagerServiceClient()
     name = f"projects/{project}/secrets/{SETTINGS_NAME}/versions/latest"
     payload = client.access_secret_version(name=name).payload.data.decode("UTF-8")
     env.read_env(io.StringIO(payload))
-    # [END cloudrun_django_secretconfig]
