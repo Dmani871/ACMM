e3ad1185a61309af4db5941455109145662ce7c8
Dmani <dmani.barnett@gmail.com>
2021-05-28 00:03:33 +0100

    Updated wsgi

--- ACMM/ACMM/wsgi.py
+++ ACMM/ACMM/wsgi.py
@@ -1,10 +1,5 @@
 import os
 from django.core.wsgi import get_wsgi_application
-from .settings.env_config import env
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ACMM.settings.development')
-
-if env('DJANGO_SETTINGS_MODULE'):
-    os.environ['DJANGO_SETTINGS_MODULE'] = env('DJANGO_SETTINGS_MODULE')
-
+os.environ['DJANGO_SETTINGS_MODULE']=os.getenv('DJANGO_SETTINGS_MODULE', 'ACMM.settings.development')
 application = get_wsgi_application()
