25f5b3bbb95e9c501cb1f139dee09cb91d1105bf
Dmani <dmani.barnett@gmail.com>
2021-05-27 15:27:49 +0100

    Cleaned up development settings

--- ACMM/ACMM/settings/development.py
+++ ACMM/ACMM/settings/development.py
@@ -2,7 +2,13 @@ from .base import *
 DEBUG = True
 ALLOWED_HOSTS = []
 DATABASES = {
-    'default': env.db()
+    'default':{
+        'ENGINE': 'django.db.backends.postgresql',
+        'HOST': env.str('DEV_DB_HOST'),
+        'PORT': env.str('DEV_DB_PORT'),
+        'USER': env.str('DB_USER'),
+        'PASSWORD': env.str('DB_PASSWORD'),
+        'NAME': env.str('PROD_DB_NAME'),
+    }
 }
 EMAIL_CONFIG = env.email_url('EMAIL_URL')
-SECURE_SSL_REDIRECT = False
