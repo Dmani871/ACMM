bf4a1008dc286d505e52b1a51ac77dcc7b29dbd6
Dmani <dmani.barnett@gmail.com>
2021-06-02 17:47:03 +0100

    Tidy up development settings

--- ACMM/ACMM/settings/development.py
+++ ACMM/ACMM/settings/development.py
@@ -6,9 +6,8 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'HOST': env.str('DEV_DB_HOST'),
         'PORT': env.str('DEV_DB_PORT'),
-        'USER': env.str('DB_USER'),
-        'PASSWORD': env.str('DB_PASSWORD'),
-        'NAME': env.str('PROD_DB_NAME'),
+        'USER': env.str('DEV_DB_USER'),
+        'PASSWORD': env.str('DEV_DB_PASSWORD'),
+        'NAME': env.str('DEV_DB_NAME'),
     }
 }
-EMAIL_CONFIG = env.email_url('EMAIL_URL')
