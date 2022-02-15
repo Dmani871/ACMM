73cc47e07405e82a0ebfd4497f684e5408e071dc
dmanibarnett <dmani.barnett@gmail.com>
2022-02-15 12:48:04 +0000

    Added test for MenteeForm for hear about us option.

--- mentorship/tests/test_forms.py
+++ mentorship/tests/test_forms.py
@@ -289,3 +289,21 @@ class AddMenteeFormTests(TestCase):
             'mentor_help': "Help me with ...",
             'mentor_relationship': "I will build a relationship by ..."})
         self.assertFalse(form.is_valid())
+
+    def test_invalid_hear_about_us(self):
+        form = forms.MenteeForm(data={
+            'email': 'jane.doe@mail.com',
+            'first_name': 'Jane',
+            'last_name': 'Doe',
+            'sex': 'M',
+            'year_applied': 'A2',
+            'hear_about_us': 'Word of Mouth',
+            'entrance_exam_experience': ['UCAT'],
+            'interview_experience': ['P'],
+            'area_of_support': ['PS'],
+            'course': 'M',
+            'current_application':True,
+            'mentor_need': "I want a mentor because ...",
+            'mentor_help': "Help me with ...",
+            'mentor_relationship': "I will build a relationship by ..."})
+        self.assertFalse(form.is_valid())
