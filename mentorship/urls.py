b19992a0c759e309937d3c9f2c1bdc8802c313dd
dmanibarnett <dmani.barnett@gmail.com>
2022-02-15 14:40:40 +0000

    Added mentee signup page

--- mentorship/urls.py
+++ mentorship/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('thanks' ,views.thank_you_view, name='thank_you'),
     path('next_year',views.next_year_view, name='next_year'),
     path('mentor/signup',views.mentor_signup_view, name='mentor'),
+    path('mentee/signup',views.mentee_signup_view, name='mentee')
 ]
--- mentorship/views.py
+++ mentorship/views.py
@@ -1,20 +1,22 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
 from . import forms
 from django.forms import formset_factory
+
+
 def thank_you_view(request):
-    return render(request,  'mentorship/thank_you.html',{'page_title':'Thank You'})
+    return render(request, 'mentorship/thank_you.html', {'page_title': 'Thank You'})
+
 
 def next_year_view(request):
-    return render(request,  'mentorship/next_year.html',{'page_title':'Apply Next Year'})
+    return render(request, 'mentorship/next_year.html', {'page_title': 'Apply Next Year'})
+
 
 def mentor_signup_view(request):
     if request.method == "POST":
         form = forms.MentorForm(request.POST)
         if form.is_valid():
-            if form.cleaned_data['honeypot']:
-                return redirect('mentorship:thank_you')
-            profile=form.save(commit=False)
-            formset = forms.MentorQualificationFormSet(request.POST, request.FILES,instance=profile)
+            profile = form.save(commit=False)
+            formset = forms.MentorQualificationFormSet(request.POST, request.FILES, instance=profile)
             if formset.is_valid():
                 form.save()
                 formset.save()
@@ -24,4 +26,26 @@ def mentor_signup_view(request):
     else:
         form = forms.MentorForm()
         formset = forms.MentorQualificationFormSet()
-    return render(request,  'mentorship/mentor_signup.html', {'page_title':'Mentor Application',"form": form,"qualification_formset": formset})
+    return render(request, 'mentorship/mentor_signup.html',
+                  {'page_title': 'Mentor Application', "form": form, "qualification_formset": formset})
+
+
+def mentee_signup_view(request):
+    if request.method == "POST":
+        form = forms.MenteeForm(request.POST)
+        if form.is_valid():
+            if form.cleaned_data['current_application'] == "False":
+                return redirect('mentorship:next_year')
+            profile = form.save(commit=False)
+            formset = forms.MenteeQualificationFormSet(request.POST, request.FILES, instance=profile)
+            if formset.is_valid():
+                form.save()
+                formset.save()
+                return redirect('mentorship:thank_you')
+        else:
+            formset = forms.MenteeQualificationFormSet(request.POST, request.FILES)
+    else:
+        form = forms.MenteeForm()
+        formset = forms.MenteeQualificationFormSet()
+    return render(request, 'mentorship/mentee_signup.html',
+                  {'page_title': 'Mentee Application', "form": form, "qualification_formset": formset})
