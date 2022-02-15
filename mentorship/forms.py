f942658500dbf3e761051c6860b3db2d5bed1389
dmanibarnett <dmani.barnett@gmail.com>
2022-02-15 08:26:43 +0000

    Added MentorForm labels.

--- mentorship/forms.py
+++ mentorship/forms.py
@@ -5,15 +5,22 @@ from . import models
 class MentorForm(forms.ModelForm):
     area_of_support = forms.MultipleChoiceField(
         widget=forms.CheckboxSelectMultiple,
-        choices=models.SPECIALTY_CHOICES)
+        choices=models.SPECIALTY_CHOICES,
+        label="What area can you provide support in?")
     interview_experience = forms.MultipleChoiceField(
         widget=forms.CheckboxSelectMultiple,
-        choices=models.INTERVIEW_EXPERIENCE_CHOICES)
+        choices=models.INTERVIEW_EXPERIENCE_CHOICES,
+        label="What interview experience do you have?")
     entrance_exam_experience = forms.MultipleChoiceField(
         widget=forms.CheckboxSelectMultiple(),
         choices=models.ENTRANCE_EXAM_CHOICES,
+        label="What exam experience do you have?",
         required=False)
+
     class Meta:
         model = models.MentorProfile
-        exclude = ['is_active','date_joined']
-
+        exclude = ['is_active', 'date_joined']
+        labels = {
+            'year_applied': 'Qualification level prior to studying Medicine/Dentistry',
+            'hear_about_us': 'How did you hear about us?'
+        }
--- mentorship/tests/test_forms.py
+++ mentorship/tests/test_forms.py
@@ -53,3 +53,26 @@ class AddMentorFormTests(TestCase):
             'occupation': 'MD'})
         self.assertTrue(form.is_valid())
 
+    def test_labels(self):
+        form = forms.MentorForm(data={
+            'email': 'john.doe@mail.com',
+            'first_name': 'John',
+            'last_name': 'Doe',
+            'sex': 'M',
+            'year_applied': 'A2',
+            'hear_about_us': 'WM',
+            'entrance_exam_experience': ['UCAT'],
+            'interview_experience': ['P'],
+            'area_of_support': ['PS'],
+            'occupation': 'MD'})
+        self.assertIn('<label for="id_email">Email:</label>', form.as_p())
+        self.assertIn('<label for="id_first_name">First name:</label>', form.as_p())
+        self.assertIn('<label for="id_last_name">Last name:</label>', form.as_p())
+        self.assertIn('<label for="id_sex">Sex:</label>', form.as_p())
+        self.assertIn('<label for="id_year_applied">Qualification level prior to studying Medicine/Dentistry:</label>', form.as_p())
+        self.assertIn('<label for="id_hear_about_us">How did you hear about us?</label>', form.as_p())
+        self.assertIn('<label>What exam experience do you have?</label>', form.as_p())
+        self.assertIn('<label>What interview experience do you have?</label>', form.as_p())
+        self.assertIn('<label>What area can you provide support in?</label>', form.as_p())
+        self.assertIn('<label for="id_occupation">Occupation:</label>', form.as_p())
+
