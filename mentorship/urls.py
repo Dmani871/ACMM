64bd33580915cacbce2f70a0349c6150f9f774ff
dmanibarnett <dmani.barnett@gmail.com>
2022-02-15 14:14:07 +0000

    Added next year page.

--- mentorship/urls.py
+++ mentorship/urls.py
@@ -4,4 +4,5 @@ from . import views
 app_name = 'mentorship'
 urlpatterns = [
     path('thanks' ,views.thank_you_view, name='thank_you'),
+    path('next_year',views.next_year_view, name='next_year'),
 ]
--- mentorship/views.py
+++ mentorship/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def thank_you_view(request):
     return render(request,  'mentorship/thank_you.html',{'page_title':'Thank You'})
+
+def next_year_view(request):
+    return render(request,  'mentorship/next_year.html',{'page_title':'Apply Next Year'})
