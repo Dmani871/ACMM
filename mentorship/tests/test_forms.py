5ae526ec0d959b3e8feb724d632c8e57682ea645
dmanibarnett <dmani.barnett@gmail.com>
2022-02-15 12:44:50 +0000

    Added test for MenteeForm for invalid sex choice.

--- mentorship/tests/test_forms.py
+++ mentorship/tests/test_forms.py
@@ -253,3 +253,21 @@ class AddMenteeFormTests(TestCase):
             'mentor_help': "Help me with ...",
             'mentor_relationship': "I will build a relationship by ..."})
         self.assertFalse(form.is_valid())
+
+    def test_invalid_sex(self):
+        form = forms.MenteeForm(data={
+            'email': 'jane.doe@mail.com',
+            'first_name': 'Jane',
+            'last_name': 'Doe',
+            'sex': 'T',
+            'year_applied': 'A2',
+            'hear_about_us': 'WM',
+            'entrance_exam_experience': ['UCAT'],
+            'interview_experience': ['P'],
+            'area_of_support': ['PS'],
+            'course': 'M',
+            'current_application':True,
+            'mentor_need': "I want a mentor because ...",
+            'mentor_help': "Help me with ...",
+            'mentor_relationship': "I will build a relationship by ..."})
+        self.assertFalse(form.is_valid())
