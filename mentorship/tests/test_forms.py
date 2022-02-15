be5b29590521f0423471221710e62cc518ee057a
dmanibarnett <dmani.barnett@gmail.com>
2022-02-15 08:09:08 +0000

    Added MentorForm in area_of_support required.

--- mentorship/tests/test_forms.py
+++ mentorship/tests/test_forms.py
@@ -16,4 +16,16 @@ class AddMentorFormTests(TestCase):
             'area_of_support': ['PS'],
             'occupation': 'MD'})
         self.assertTrue(form.is_valid())
+    def test_area_of_support_required(self):
+        form = forms.MentorForm(data={
+            'email': 'john.doe@mail.com',
+            'first_name': 'John',
+            'last_name': 'Doe',
+            'sex': 'M',
+            'year_applied': 'A2',
+            'hear_about_us': 'WM',
+            'entrance_exam_experience': ['UCAT'],
+            'interview_experience': ['P'],
+            'occupation': 'MD'})
+        self.assertFalse(form.is_valid())
 
