d9093e61917e78fc1a578a692348fe4b905fc0c7
dmanibarnett <dmani.barnett@gmail.com>
2022-02-23 05:41:03 +0000

    Commented export_as_csv

--- mentorship/admin.py
+++ mentorship/admin.py
@@ -14,14 +14,18 @@ from django.contrib.contenttypes.models import ContentType
 
 @admin.action(description='Export Selected Profiles')
 def export_as_csv(self, request, queryset):
+    # gets meta data about model
     meta = self.model._meta
     # gets all the field names from the model
     field_names = [field.name for field in meta.fields]
     # form the cols names
     column_names = field_names + ["qualifications"]
+    # tells browser to treat response as an attachment
     response = HttpResponse(content_type='text/csv')
     response['Content-Disposition'] = 'attachment; filename={}.csv'.format(meta)
+    # returns writer object that writes to the response
     writer = csv.writer(response)
+    # write cols
     writer.writerow(column_names)
     # for obj in selected objects
     for obj in queryset:
@@ -85,6 +89,88 @@ class MenteeQualificationInline(admin.TabularInline):
     can_delete = True
 
 
+def generate_matches(mentors, mentees):
+    mentee_preferences = defaultdict(dict)
+    for mentee in mentees:
+        rankings = defaultdict(dict)
+        for mentor in mentors:
+            ranking = 10
+            # if both the mentee and mentor applied at the same stage(grad or post-18) double ranking
+            if mentor.year_applied == mentee.year_applied:
+                ranking *= 2
+            # if the sex are not the same decrease ranking by 20%
+            if mentor.sex != mentee.sex:
+                ranking *= 0.8
+            # counts the overlapping area_of_support for both the mentee and mentor
+            support_factor = len(np.intersect1d(mentor.area_of_support, mentee.area_of_support))
+            # counts how much interview experience the mentor had
+            interview_factor = len(mentor.interview_experience)
+            # counts the overlapping exam experience
+            exam_factor = len(np.intersect1d(mentor.entrance_exam_experience, mentee.entrance_exam_experience))
+
+            # ensures the mentor can support the mentee based on their experiences
+            if ('EE' in mentee.area_of_support and exam_factor == 0) or (
+                    'I' in mentee.area_of_support and interview_factor == 0):
+                support_factor = 0
+            # amplifies the exam factor if they need support for entrance exams
+            if 'EE' in mentee.area_of_support:
+                exam_factor *= 10
+            # amplifies the interview factor if they need support for interviews
+            if 'I' in mentee.area_of_support:
+                interview_factor *= 10
+            # calculates the mentor factor
+            mentor_factor = 1 + ((exam_factor + interview_factor) * 5)
+            # calculates the overall ranking
+            ranking = ranking * support_factor * mentor_factor
+            # if the mentor can be of use add to the ma
+            if ranking > 0:
+                rankings[mentor.id] = ranking
+            mentee_preferences[mentee.id] = rankings
+
+    matches_df = pd.DataFrame(mentee_preferences).fillna(-1)
+    # mentee preferneces
+    mentee_pref_df = matches_df
+    mentor_list = matches_df.index.tolist()
+    mentor_dict = dict.fromkeys(mentor_list, 0)
+    waiting_list = defaultdict(list)
+    try:
+        while bool(mentor_dict):
+            for k, v in mentor_dict.items():
+                # chose the mentor with the highest rank
+                waiting_list[mentee_pref_df.loc[k].nlargest().index[v]].append(k)
+                mentor_dict[k] += 1
+            new_mentor_dict = defaultdict(int)
+            for k, v in waiting_list.items():
+                if len(v) > 1:
+                    v_t = mentee_pref_df[k].filter(items=v).sort_values(ascending=False).index.tolist()
+                    print(v_t)
+                    for x in v_t[1:]:
+                        new_mentor_dict[x] = mentor_dict[x]
+                    waiting_list[k] = v_t[:1]
+            mentor_dict = new_mentor_dict
+    except IndexError:
+        pass
+    return waiting_list
+
+
+def save_matches(request, matches, ct):
+    for k, v in matches.items():
+        mentor = MentorProfile.objects.get(id=v[0])
+        mentee = MenteeProfile.objects.get(id=k)
+        mentee.mentor = mentor
+        LogEntry.objects.log_action(
+            user_id=request.user.id,
+            content_type_id=ct.pk,
+            object_id=mentee.pk,
+            object_repr=mentee.__str__(),
+            action_flag=CHANGE,
+            change_message="Added mentor match")
+        mentee.save()
+    l = LogEntry(user_id=request.user.id, action_flag=CHANGE, content_type_id=ct.pk,
+                 change_message="Mentee-Mentor matches ran")
+    l.save()
+
+
 class MenteeAdmin(admin.ModelAdmin):
     inlines = [
         MenteeQualificationInline,
@@ -92,7 +178,7 @@ class MenteeAdmin(admin.ModelAdmin):
     exclude = [""]
     form = MenteeForm
     list_filter = ['course', 'date_joined', 'accepted', 'year_applied']
-    list_display = ['email', 'first_name', 'last_name','area_of_support','mentor_link']
+    list_display = ['email', 'first_name', 'last_name', 'area_of_support', 'mentor_link']
     fieldsets = [
         ('Personal Information', {'fields': ['first_name', 'last_name', 'email', 'sex']}),
         ('Background Information',
@@ -111,7 +197,10 @@ class MenteeAdmin(admin.ModelAdmin):
             return format_html('<a href="{}">{}</a>', admin_url, mentor)
         else:
             return None
+
     mentor_link.short_description = 'Mentor'
+
+
     @admin.action(description='Export Matches Info')
     def export_matches_info(self, request, queryset):
         ct = ContentType.objects.get_for_model(queryset.model)
@@ -124,11 +213,11 @@ class MenteeAdmin(admin.ModelAdmin):
                         "mentee.interview_experience", "mentor.interview_experience",
                         "mentee.entrance_exam_experience", "mentor.entrance_exam_experience"]
         response = HttpResponse(content_type='text/csv')
-        response['Content-Disposition'] = 'attachment; filename=mentorships-matches.csv'
+        response['Content-Disposition'] = 'attachment; filename=mentorship-matches.csv'
         writer = csv.writer(response)
         writer.writerow(column_names)
         for mentee in queryset:
-            if mentee.mentor != None:
+            if mentee.mentor:
                 mentor = mentee.mentor
                 row_contents = [
                     mentee.id, mentor.id,
@@ -149,85 +238,6 @@ class MenteeAdmin(admin.ModelAdmin):
                     change_message="Exported Matches Info")
         return response
 
-    def generate_matches(self, mentors, mentees):
-        mentee_preferences = defaultdict(dict)
-        for mentee in mentees:
-            rankings = defaultdict(dict)
-            for mentor in mentors:
-                ranking = 10
-                # if both the mentee and mentor applied at the same stage(grad or post-18) double ranking
-                if mentor.year_applied == mentee.year_applied:
-                    ranking *= 2
-                # if the sex are not the same decrease ranking by 20%
-                if mentor.sex != mentee.sex:
-                    ranking *= 0.8
-                # counts the overlapping area_of_support for both the mentee and mentor
-                support_factor = len(np.intersect1d(mentor.area_of_support, mentee.area_of_support))
-                # counts how much interview experience the mentor had
-                interview_factor = len(mentor.interview_experience)
-                # counts the overlapping exam experience
-                exam_factor = len(np.intersect1d(mentor.entrance_exam_experience, mentee.entrance_exam_experience))
-
-                # ensures the mentor can support the mentee based on their experiences
-                if ('EE' in mentee.area_of_support and exam_factor == 0) or (
-                        'I' in mentee.area_of_support and interview_factor == 0):
-                    support_factor = 0
-                # amplifies the exam factor if they need support for entrance exams
-                if 'EE' in mentee.area_of_support:
-                    exam_factor *= 10
-                # amplifies the interview factor if they need support for interviews
-                if 'I' in mentee.area_of_support:
-                    interview_factor *= 10
-                # calculates the mentor factor
-                mentor_factor = 1 + ((exam_factor + interview_factor) * 10)
-                # calculates the overall ranking
-                ranking = ranking * support_factor * mentor_factor
-                # if the mentor can be of use add to the ma
-                if ranking > 0:
-                    rankings[mentor.id] = ranking
-                mentee_preferences[mentee.id] = rankings
-
-        matches_df = pd.DataFrame(mentee_preferences).fillna(-1)
-        # mentee preferneces
-        mentee_pref_df = matches_df
-        mentor_list = matches_df.index.tolist()
-        mentor_dict = dict.fromkeys(mentor_list, 0)
-        waiting_list = defaultdict(list)
-        try:
-            while bool(mentor_dict):
-                for k, v in mentor_dict.items():
-                    # chose the mentor with the highest rank
-                    waiting_list[mentee_pref_df.loc[k].nlargest().index[v]].append(k)
-                    mentor_dict[k] += 1
-                new_mentor_dict = defaultdict(int)
-                for k, v in waiting_list.items():
-                    if len(v) > 1:
-                        v_t = mentee_pref_df[k].filter(items=v).sort_values(ascending=False).index.tolist()
-                        print(v_t)
-                        for x in v_t[1:]:
-                            new_mentor_dict[x] = mentor_dict[x]
-                        waiting_list[k] = v_t[:1]
-                mentor_dict = new_mentor_dict
-        except IndexError:
-            pass
-        return waiting_list
-
-    def save_matches(self,request,matches,ct):
-        for k, v in matches.items():
-            mentor = MentorProfile.objects.get(id=v[0])
-            mentee = MenteeProfile.objects.get(id=k)
-            mentee.mentor = mentor
-            LogEntry.objects.log_action(
-                user_id=request.user.id,
-                content_type_id=ct.pk,
-                object_id=mentee.pk,
-                object_repr=mentee.__str__(),
-                action_flag=CHANGE,
-                change_message="Added mentor match")
-            mentee.save()
-        l = LogEntry(user_id=request.user.id, action_flag=CHANGE, content_type_id=ct.pk,
-                     change_message="Mentee-Mentor matches ran")
-        l.save()
     @admin.action(description='Assign a mentor to each mentee')
     def assign_mentor(self, request, queryset):
         ct = ContentType.objects.get_for_model(queryset.model)
@@ -237,12 +247,10 @@ class MenteeAdmin(admin.ModelAdmin):
         unmatched_mentees = queryset
         unmatched_medicine_mentees = unmatched_mentees.filter(course__startswith='M')
         unmatched_dentistry_mentees = unmatched_mentees.difference(unmatched_medicine_mentees)
-        medicine_matches = self.generate_matches(available_medicine_mentors, unmatched_medicine_mentees)
-        dentistry_matches = self.generate_matches(available_dentistry_mentors, unmatched_dentistry_mentees)
-        self.save_matches(request,medicine_matches,ct)
-        self.save_matches(request,dentistry_matches,ct)
-
-
+        medicine_matches = generate_matches(available_medicine_mentors, unmatched_medicine_mentees)
+        dentistry_matches = generate_matches(available_dentistry_mentors, unmatched_dentistry_mentees)
+        save_matches(request, medicine_matches, ct)
+        save_matches(request, dentistry_matches, ct)
 
     actions = [export_as_csv, "assign_mentor", "export_matches_info"]
 
