5050b1db7669a065ba3baa8c81dd264fd2817d65
dmanibarnett <dmani.barnett@gmail.com>
2022-02-26 12:40:52 +0000

    Added custom mentor simple filter

--- mentorship/filters.py
+++ mentorship/filters.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.utils.translation import gettext_lazy as _
 
-class MentorListFilter(admin.AllValuesFieldListFilter):
+class MentorListFilter(admin.SimpleListFilter):
 
     title = _('has mentor')
 
