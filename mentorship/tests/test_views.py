2de71fe7db1e9a967d9efd5bbe3869eddb316c07
dmanibarnett <dmani.barnett@gmail.com>
2022-03-21 12:04:09 +0000

    Added application view test

--- mentorship/tests/test_views.py
+++ mentorship/tests/test_views.py
@@ -30,3 +30,19 @@ class NextYearViewTest(TestCase):
         response = self.client.get(reverse("mentorship:next_year"))
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'mentorship/next_year.html')
+
+
+class ApplicationsViewTest(TestCase):
+    def test_view_url_exists_at_desired_location(self):
+        response = self.client.get('/mentorship/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_url_accessible_by_name(self):
+        response = self.client.get(reverse("mentorship:index"))
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_uses_correct_template(self):
+        response = self.client.get(reverse("mentorship:index"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'mentorship/applications.html')
+# TODO:Selenium test
