b4411afcbfe4db3daaef3b7dca223d833bc351df
dmanibarnett <dmani.barnett@gmail.com>
2022-03-21 12:09:56 +0000

    Added mentor view test

--- mentorship/tests/test_views.py
+++ mentorship/tests/test_views.py
@@ -1,6 +1,19 @@
 from django.test import TestCase
 from django.urls import reverse
 
+class ApplicationsViewTest(TestCase):
+    def test_view_url_exists_at_desired_location(self):
+        response = self.client.get('/mentorship/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_url_accessible_by_name(self):
+        response = self.client.get(reverse("mentorship:index"))
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_uses_correct_template(self):
+        response = self.client.get(reverse("mentorship:index"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'mentorship/applications.html')
 
 class ThankYouViewTest(TestCase):
     def test_view_url_exists_at_desired_location(self):
@@ -32,17 +45,17 @@ class NextYearViewTest(TestCase):
         self.assertTemplateUsed(response, 'mentorship/next_year.html')
 
 
-class ApplicationsViewTest(TestCase):
+class MentorViewTest(TestCase):
     def test_view_url_exists_at_desired_location(self):
-        response = self.client.get('/mentorship/')
+        response = self.client.get('/mentorship/mentor/signup')
         self.assertEqual(response.status_code, 200)
 
     def test_view_url_accessible_by_name(self):
-        response = self.client.get(reverse("mentorship:index"))
+        response = self.client.get(reverse("mentorship:mentor"))
         self.assertEqual(response.status_code, 200)
 
     def test_view_uses_correct_template(self):
-        response = self.client.get(reverse("mentorship:index"))
+        response = self.client.get(reverse("mentorship:mentor"))
         self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'mentorship/applications.html')
+        self.assertTemplateUsed(response, 'mentorship/mentor_signup.html')
 # TODO:Selenium test
